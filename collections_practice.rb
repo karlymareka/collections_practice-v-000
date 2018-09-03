20d2559c868830040d7215dd70c06a2386b93efb
Karly Mareka <karly.mareka@gmail.com>
2018-09-03 17:57:11 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,13 @@
+def sort_array_asc(array)
+  array.sort
+end 
 
+def sort_array_desc(array)
+  array.sort!
+  array.reverse!
+end
+
+sort_array_char_count(array)
+  array.sort! {|left, right| left.length <=> right.length}
+end 
+end 
